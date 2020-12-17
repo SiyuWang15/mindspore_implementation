9127fabe989e252824ce83d65026ca328c5561fc
SiyuWang15 <thuwangsy@gmail.com>
2020-12-17 21:43:38 +0800

    update

--- train/src/dataset.py
+++ train/src/dataset.py
@@ -146,7 +146,7 @@ def ofdm_simulate(codeword, channelResponse, SNRdb, mu, CP_flag, K, P, CP, pilot
 
 def MIMO(X, HMIMO, SNRdb,flag,P):
     P = P * 2
-    Pilot_file_name = './dataset/Pilot_' + str(P)
+    Pilot_file_name = '../dataset/Pilot_' + str(P)
     bits = np.loadtxt(Pilot_file_name, delimiter = ',')
     # if os.path.isfile(Pilot_file_name):
     #     bits = np.loadtxt(Pilot_file_name, delimiter=',')
@@ -230,13 +230,13 @@ class RandomDataset():
         # return 2000
 
 def get_YH_data_random(mode, Pn):
-    N1 = 2000
-    data1 = open('./dataset/H_val.bin','rb')
+    N1 = 320000
+    data1 = open('../dataset/H.bin','rb')
     H1 = struct.unpack('f'*2*2*2*32*N1,data1.read(4*2*2*2*32*N1))
     H1 = np.reshape(H1,[N1,2,4,32])
     H_tra = H1[:,1,:,:]+1j*H1[:,0,:,:]   # time-domain channel for training 
 
-    data2 = open('./dataset/H_val.bin','rb')
+    data2 = open('../dataset/H_val.bin','rb')
     H2 = struct.unpack('f'*2*2*2*32*2000,data2.read(4*2*2*2*32*2000))
     H2 = np.reshape(H2,[2000,2,4,32])
     H_val = H2[:,1,:,:]+1j*H2[:,0,:,:] 
@@ -245,7 +245,7 @@ def get_YH_data_random(mode, Pn):
     return trainset, valset
 
 def get_val_data(Pn, mode):
-    H_path = os.path.join('./dataset/H_val.bin')
+    H_path = os.path.join('../dataset/H_val.bin')
     H_data = open(H_path, 'rb')
     H = struct.unpack('f'*2*2*2*32*2000, H_data.read(4*2*2*2*32*2000))
     H = np.reshape(H, [2000, 2, 4, 32]).astype('float32')
